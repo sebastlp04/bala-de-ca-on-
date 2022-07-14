31c25103d495de56f337b243b78888d7009dd374
sebastlp04 <104152517+sebastlp04@users.noreply.github.com>
2022-07-14 18:00:44 -0500

    Add files via upload

---
+++ PRO-C22-reference-link/js/cannonBall.js
@@ -0,0 +1,34 @@
+class Cannonball{
+        constructor(x,y)
+        {
+            this.x = x;
+            this.y = y;
+            this.r = 30;
+
+            var options = {
+                isStatic : true
+            };
+
+            this.image = loadImage("./assets/cannonball.png");
+            this.body = Bodies.circle(x,y,this.r,options);
+            World.add(world,this.body);
+
+        }
+        display(){
+            var pos = this.body.position;
+
+            push();
+            imageMode(CENTER);
+            image(this.image,pos.x,pos.y,this.r,this.r);
+            pop();
+        }
+        shoot(){
+           var newAngle = cannon.angle - 28;
+           newAngle = newAngle*(3.14/180);
+           var velocity = p5.Vector.fromAngle(newAngle);
+           velocity.mult(0.5);
+            Matter.Body.setStatic(this.body,false);
+            Matter.Body.setVelocity(this.body,{x:velocity.x * (180/3.14),y:velocity.y * (180/3.14)});
+
+        }
+}
